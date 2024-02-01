3664919b48e9abf2bc263c49cc7bbde2ce9d1deb
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-01-31 20:32:18 -0600

    CnsumirApi

--- rutas/usuariosRutas.js
+++ rutas/usuariosRutas.js
@@ -1,83 +1,52 @@
-var ruta=require("express").Router();
+var ruta = require("express").Router();
 var subirArchivo = require("../middlewares/subirArchivo");
-var {mostrarUsuarios, nuevoUsuario, modificarUsuario, buscarPorID, borrarUsuario, verificarCredenciales}=require("../bd/usuariosBD");
+var { mostrarUsuarios, nuevoUsuario, modificarUsuario, buscarPorID, borrarUsuario } = require("../bd/usuariosBD");
 const fs = require('fs').promises;
 
-ruta.get('/', (req, res) => {
-  res.render('usuarios/login');
+ruta.get('/', async (req, res) => {
+  var usuarios = await mostrarUsuarios();
+  res.render("usuarios/mostrar", { usuarios });
 });
 
-ruta.post('/login', async (req, res) => {
-  const { usuario, password } = req.body;
+ruta.get("/nuevousuario", async (req, res) => {
+  res.render("usuarios/nuevo");
+});
 
-  const user = await verificarCredenciales(usuario, password);
+ruta.post("/nuevousuario", subirArchivo(), async (req, res) => {
+  req.body.foto = req.file.originalname;
+  var error = await nuevoUsuario(req.body);
+  res.redirect("/");
+});
 
-  if (user) {
-    req.session.isLoggedIn = true;
-    res.redirect('/mostrar');
+ruta.get("/editar/:id", async (req, res) => {
+  var user = await buscarPorID(req.params.id);
+  res.render("usuarios/modificar", { user });
+});
+
+ruta.post("/editar", subirArchivo(), async (req, res) => {
+  if (req.file != undefined) {
+    req.body.foto = req.file.originalname;
   } else {
-    res.render('usuarios/login', { error: 'Credenciales incorrectas' });
+    req.body.foto = req.body.fotoVieja;
   }
-});
-
-ruta.get("/logout", (req,res)=>{ 
-  req.session=null;
+  var error = await modificarUsuario(req.body);
   res.redirect("/");
-
-});
-
-ruta.get("/mostrar",async(req,res)=>{
-    var usuarios = await mostrarUsuarios();
-    res.render("usuarios/mostrar",{usuarios});
-  
-});
-
-ruta.get("/nuevousuario", async(req,res)=>{
-    res.render("usuarios/nuevo");
-
-});
-
-ruta.post("/nuevousuario",subirArchivo(),async(req, res)=>{
-    req.body.foto=req.file.originalname;
-    var error=await nuevoUsuario(req.body);
-    res.redirect("/");
-
-});
-
-ruta.get("/editar/:id",async(req, res)=>{
-    var user=await buscarPorID(req.params.id);
-    res.render("usuarios/modificar",{user});
-
-});
-
-ruta.post("/editar",subirArchivo(), async (req, res) => { 
-  if(req.file!=undefined){
-    req.body.foto=req.file.originalname;
-  }
-  else{
-    req.body.foto=req.body.fotoVieja;
-
-  }
-  var error=await modificarUsuario(req.body);
-  res.redirect("/");
-  
 });
 
 ruta.get("/borrar/:id", async (req, res) => {
-    try {
-      const user = await buscarPorID(req.params.id);
-      if (!user) {
-        return res.status(404).send("Usuario no encontrado");
-      }
-      await fs.unlink(`./web/images/${user.foto}`);
-      await borrarUsuario(req.params.id);
-  
-      res.redirect("/");
-    } catch (error) {
-      console.error('Error al borrar la foto o usuario:', error);
-      res.status(500).send("Error al borrar la foto o usuario");
+  try {
+    const user = await buscarPorID(req.params.id);
+    if (!user) {
+      return res.status(404).send("Usuario no encontrado");
     }
-  });
+    await fs.unlink(`./web/images/${user.foto}`);
+    await borrarUsuario(req.params.id);
 
-  
-module.exports=ruta;
+    res.redirect("/");
+  } catch (error) {
+    console.error('Error al borrar la foto o usuario:', error);
+    res.status(500).send("Error al borrar la foto o usuario");
+  }
+});
+
+module.exports = ruta;
